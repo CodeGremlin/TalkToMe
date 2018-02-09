204fcf024b2c9d4542cfeaa1851fcdde25f51f62
Rick Williams <risingsun66@gmail.com>
2018-02-09 16:44:01 -0500

    project environment is set up with all Podfiles added gradient to implemented gestures to the first view

--- TalkToMe/Controller/ChannelVC.swift
+++ TalkToMe/Controller/ChannelVC.swift
@@ -1,25 +1,29 @@
 //
-//  ViewController.swift
+//  ChannelVC.swift
 //  TalkToMe
 //
-//  Created by Rick Williams on 1/3/18.
+//  Created by Rick Williams on 2/7/18.
 //  Copyright © 2018 Rick Williams. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChannelVC: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+   
 
 }
-
---
+++ TalkToMe/Controller/ChatVC.swift
@@ -0,0 +1,28 @@
+//
+//  ChatVC.swift
+//  TalkToMe
+//
+//  Created by Rick Williams on 2/7/18.
+//  Copyright © 2018 Rick Williams. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+   //outlets
+    
+    @IBOutlet weak var menuBtn: UIButton!
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+
+    
+    
+
+   
+}
